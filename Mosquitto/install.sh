e2e6ff24fe802febb6e437bd101818ce0b522945
Suren Sritharan <suren3141@gmail.com>
2018-06-18 01:29:17 +0530

    Add files via upload

--- Mosquitto/install.sh
+++ Mosquitto/install.sh
@@ -1,4 +1,6 @@
 sudo apt-get install mosquitto
 git clone https://github.com/jpmens/mosquitto-auth-plug
+sudo apt-get install mysql-server
+
 cp mosquitto-auth-plug/config.mk.in mosquitto-auth-plug/config.mk
 
