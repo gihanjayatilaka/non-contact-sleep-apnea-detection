f64acc7060b9cd5cdf2575dc2215964356983800
punk <pankayaraj1995@gmail.com>
2018-06-18 00:37:26 +0530

    pankayaraj commit

---
+++ SERVER/clients/init_model.py
@@ -0,0 +1,16 @@
+
+from .models import Client_buffer_recv
+import datetime
+
+
+def init_on_register(user):
+
+    pass
+    '''
+    b = Client_buffer_recv()
+    b.user = user
+    b.data = ""
+    b.timestamp = datetime.datetime.now()
+    b.save()
+    '''
+
