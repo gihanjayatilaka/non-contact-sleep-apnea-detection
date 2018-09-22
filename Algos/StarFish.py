2ca4dcd06a0e63237921ee88c25defbf79350f36
Gihan Chanaka Jayatilaka <gihanchanaka@gmail.com>
2018-09-22 15:46:39 +0530

    Add files via upload

--- Algos/StarFish.py
+++ Algos/StarFish.py
@@ -8,6 +8,9 @@ import argparse
 import cv2
 import sys
 from time import sleep
+import matplotlib
+matplotlib.use('svg',warn=False,force=True)
+import matplotlib.pyplot as plt
 
 def mouse_callback(event, x, y, flags, params):
     global centerBelly_X
@@ -32,6 +35,8 @@ if __name__== "__main__":
 
     ROWS=int(sys.argv[4])
     COLS=int(sys.argv[5])
+
+
 
     cap = cv2.VideoCapture(FILENAME)
 
@@ -62,38 +67,49 @@ if __name__== "__main__":
                 cv2.destroyWindow("OriginalVideo")
 
 
-                edgeVideo=np.zeros((END_FRAME-START_FRAME+1,frame.shape[0],frame.shape[1]))
-                cogVideo=np.zeros((END_FRAME-START_FRAME+1,ROWS,COLS,2),dtype=np.int32)
+                #edgeVideo=np.zeros((END_FRAME-START_FRAME+1,frame.shape[0],frame.shape[1]))
+                #cogVideo=np.zeros((END_FRAME-START_FRAME+1,2),dtype=np.int32)
+                timeSeries=np.zeros((END_FRAME-START_FRAME))
                 ret=False
                 while not ret:
                     ret, frame = cap.read()
 
-        i=0
-        while True:
-            WINDOW_HEIGHT=5
-            THRESHOLD=50
-            currentMean = np.zeros((3), dtype=np.float32)
-            nextMean = np.zeros((3), dtype=np.float32)
+            i=0
+            while True:
+                WINDOW_HEIGHT=5
+                THRESHOLD=50
+                currentMean = np.zeros((3), dtype=np.float32)
+                nextMean = np.zeros((3), dtype=np.float32)
 
-            for x in range(WINDOW_HEIGHT):
-                currentMean[:]+=frame[centerBelly_Y-(i+x),centerBelly_X,:]
-                nextMean[:] += frame[centerBelly_Y - (i + x+1), centerBelly_X, :]
+                for x in range(WINDOW_HEIGHT):
+                    currentMean[:]+=frame[centerBelly_Y-(i+x),centerBelly_X,:]
+                    nextMean[:] += frame[centerBelly_Y - (i + x+1), centerBelly_X, :]
 
-            if np.sum(np.abs(nextMean-currentMean)) < THRESHOLD:
-                i+=1
-            else:
-                break
+                if np.sum(np.abs(nextMean-currentMean)) < THRESHOLD:
+                    i+=1
+                else:
+                    break
 
-            if centerBelly_Y - (i + 4+1) ==0:
-                break
+                if centerBelly_Y - (i + 4+1) ==0:
+                    break
 
-        cv2.circle(frame, (centerBelly_X, centerBelly_Y), 2, (0, 0, 0), 2)
+            cv2.circle(frame, (centerBelly_X, centerBelly_Y), 2, (0, 0, 0), 2)
 
-        for ii in range(i):
-            cv2.circle(frame, (centerBelly_X, centerBelly_Y-ii), 1, (256, 0, 0), 1)
+            for ii in range(i):
+                cv2.circle(frame, (centerBelly_X, centerBelly_Y-ii), 1, (256, 0, 0), 1)
 
-        cv2.imshow("OriginalVideo", frame)
-        cv2.waitKey(1)
+            timeSeries[t]=i
+
+
+            cv2.imshow("OriginalVideo", frame)
+            cv2.waitKey(1)
+
+    #After processing the video
+    print(timeSeries)
+    plt.figure()
+    plt.plot(np.arange(0,timeSeries.shape[0]),timeSeries)
+    plt.show()
+    cv2.waitKey(1000)
 
 
 
